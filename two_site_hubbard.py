feed15b5dbc05ca696ab5fc6fa61f5f85039d932
Chandeepa Dissanayake <chandeepadissanayake@gmail.com>
2024-06-03 01:54:28 +0530

    Changed the Hamiltonian to Time-Dependent System.

--- two_site_hubbard.py
+++ two_site_hubbard.py
@@ -1,19 +1,21 @@
 from simuq import QSystem
 from simuq import Qubit
 from simuq.qutip import QuTiPProvider
-# from simuq.ibm.ibm_provider import IBMProvider
+import numpy as np
 
 n_qubits = 2
 qs = QSystem()
 q1, q2 = Qubit(qs), Qubit(qs)
 
-J = 4
-U = 3
+J = 1
+U_0 = -5
+U_f = 5
 
-T = 10
+T = 4
+M = 30
 
-H = -J * (q1.X + q2.X) + U * (q1.Z * q2.Z)
-qs.add_evolution(H, T)
+H = lambda t: -J * (q1.X + q2.X) + ((1 - t / T) * U_0 + (t / T) * U_f) * (q1.Z * q2.Z)
+qs.add_td_evolution(H, np.linspace(0, T, M))
 
 qpp = QuTiPProvider()
 qpp.compile(qs)
