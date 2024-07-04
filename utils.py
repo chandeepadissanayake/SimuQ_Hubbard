a13f93f06ad773cfb0a7e5c336eace3fb510da4f
Chandeepa Dissanayake <chandeepadissanayake@gmail.com>
2024-07-04 18:46:45 +0530

    Changed visualization function to include <3D graphs as well.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 from mayavi import mlab
 
 
@@ -12,29 +13,33 @@ def visualize_lattice(G, dim):
     :return: None
     """
 
-    G_c = nx.convert_node_labels_to_integers(G)  # Convert integer coordinate node based graph to an index based node
-    # graph.
-    pos = nx.spring_layout(G_c, dim=dim, seed=1001)  # Converts the integral nodes to coordinates in 3D plane (
-    # position nodes) using Fruchterman-Reingold force-directed algorithm (Documentation:
-    # https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html)
-    xyz = np.array([pos[v] for v in sorted(G_c)])  # Sorts the 3D-plane coordinates (nodes) for plotting. This is
-    # merely a requirement in mayavi plotting.
-    scalars = np.array(list(G_c.nodes())) + 5  # Scalar value per each node, to modulate their size and color.
+    if dim < 3:
+        nx.draw(G)
+        plt.show()
+    else:
+        G_c = nx.convert_node_labels_to_integers(G)  # Convert integer coordinate node based graph to an index based node
+        # graph.
+        pos = nx.spring_layout(G_c, dim=dim, seed=1001)  # Converts the integral nodes to coordinates in 3D plane (
+        # position nodes) using Fruchterman-Reingold force-directed algorithm (Documentation:
+        # https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html)
+        xyz = np.array([pos[v] for v in sorted(G_c)])  # Sorts the 3D-plane coordinates (nodes) for plotting. This is
+        # merely a requirement in mayavi plotting.
+        scalars = np.array(list(G_c.nodes())) + 5  # Scalar value per each node, to modulate their size and color.
 
-    mlab.figure()  # Starts the visualization using Mayavi
-    pts = mlab.points3d(
-        xyz[:, 0],
-        xyz[:, 1],
-        xyz[:, 2],
-        scalars,
-        scale_factor=0.1,
-        scale_mode="none",
-        colormap="Blues",
-        resolution=20,
-    )  # Documentation: https://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html#points3d
-    pts.mlab_source.dataset.lines = np.array(list(G_c.edges()))  # Sets all edges as lines in the 3D space.
-    tube = mlab.pipeline.tube(pts, tube_radius=0.01)  # Converts the edges to a tube-like visual structure
-    mlab.pipeline.surface(tube, color=(0.8, 0.8, 0.8))  # Performs the actual visualize of edges as tubes on the window (scene)
-    mlab.orientation_axes()  # Creates 3D axes and marks the coordinates
+        mlab.figure()  # Starts the visualization using Mayavi
+        pts = mlab.points3d(
+            xyz[:, 0],
+            xyz[:, 1],
+            xyz[:, 2],
+            scalars,
+            scale_factor=0.1,
+            scale_mode="none",
+            colormap="Blues",
+            resolution=20,
+        )  # Documentation: https://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html#points3d
+        pts.mlab_source.dataset.lines = np.array(list(G_c.edges()))  # Sets all edges as lines in the 3D space.
+        tube = mlab.pipeline.tube(pts, tube_radius=0.01)  # Converts the edges to a tube-like visual structure
+        mlab.pipeline.surface(tube, color=(0.8, 0.8, 0.8))  # Performs the actual visualize of edges as tubes on the window (scene)
+        mlab.orientation_axes()  # Creates 3D axes and marks the coordinates
 
-    mlab.show()  # This blocks the event loop.
+        mlab.show()  # This blocks the event loop.
