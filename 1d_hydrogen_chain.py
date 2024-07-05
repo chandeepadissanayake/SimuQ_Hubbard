62aa2853096f7cf3bee53b224bd8d1a4a47ac5a0
Chandeepa Dissanayake <chandeepadissanayake@gmail.com>
2024-07-05 17:42:18 +0530

    Removed Fermion object based simulation as it is wrong. Just uses Pauli implementation.

--- 1d_hydrogen_chain.py
+++ 1d_hydrogen_chain.py
@@ -1,25 +1,17 @@
 """Performs the simulation of the 1D Hydrogen Chain (Hubbard Model). We consider a chain with 2 Hydrogen atoms.
 
-This example is written in second quantization formulation.
+This example is written in Pauli gates formulation.
 
 The values of z_t and z_u are set arbitrarily. Increase the time period for simulation as required. We rely on the
 classical QuTiP simulator merely for the convenience. Users can choose to utilize other available vendors as
-necessary.
-
-IMPORTANT: Creator-Annihilator implementation in SimuQ Implementation has an apparent bug in it. Check the PR:
-https://github.com/PicksPeng/SimuQ/pull/37. Incorporate the relevant changes in this PR to your installation of SimuQ
-to fix.
-
-IMPORTANT: Even after fixing this issue, the creator-annihilator formulation doesn't seem to work well in SimuQ. It is
-advisable to try the Pauli gates based implementation for reliable results."""
+necessary."""
 
 __author__ = "Chandeepa Dissanayake"
 
 import networkx as nx
-from simuq import QSystem, Fermion
+from simuq import QSystem, Qubit
 import helpers
 import utils
-
 
 # The number of Hydrogen atoms in the chain.
 N = 2
@@ -44,18 +36,14 @@ G = nx.convert_node_labels_to_integers(G)
 
 # SimuQ Stuff
 qs = QSystem()
-# We need 2N Fermions as each site can have 1 spin-up fermion and 1 spin-down fermion.
-f = [Fermion(qs) for _ in range(2 * N)]
+q = [Qubit(qs) for _ in range(N)]
 
 # Starts building the Hamiltonian.
 H_t = 0
 H_u = 0
 for i, j in G.edges():
-    i_up, i_down = f[i * 2], f[(i * 2) + 1]
-    j_up, j_down = f[j * 2], f[(j * 2) + 1]
-
-    H_t += (i_up.c * j_up.a + j_up.c * i_up.a) + (i_down.c * j_down.a + j_down.c * i_down.a)
-    H_u += i_up.c * i_up.a * i_down.c * i_down.a
+    H_t += q[i].X + q[j].X
+    H_u += q[i].Z * q[j].Z
 
 # Build the Time Dependent Hamiltonian
 H = lambda t: -z_t * H_t + ((1 - t / T) * z_u0 + (t / T) * z_uf) * H_u
@@ -63,7 +51,7 @@ H = lambda t: -z_t * H_t + ((1 - t / T) * z_u0 + (t / T) * z_uf) * H_u
 # Simulate the Hamiltonian H over time period T using QuTiP simulator
 results = helpers.simulate_on_qutip(qs, H, T, time_dependent=True, M=M)
 # Print the results in general format.
-helpers.print_simulator_results(results, 2 * N)
+helpers.print_simulator_results(results, N)
 
 # Visualize the lattice structure in 3D.
 utils.visualize_lattice(G, 1)
